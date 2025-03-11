48470822e2cc3ad0d6d21f12e8f236a528ae1d8e
alazythe <eexepts@gmail.com>
2025-03-11 15:46:49 +0100

    change

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include "add.h"
+#include <cstddef>
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <ostream>
 
 int main() {
-    std::cout << add(3, 5) << "\n";
+    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-    return 0;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Bullshit", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 }
