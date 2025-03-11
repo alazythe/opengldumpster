bc0234152e8271708bfa44b6990dd5cfb94a62b4
alazythe <eexepts@gmail.com>
2025-03-11 19:47:22 +0100

    glad fix

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 #include <GLFW/glfw3.h>
 #include <ostream>
 
-int main() {
-    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
+int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -21,5 +20,26 @@ int main() {
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while(glfwWindowShouldClose(window) == 0) {
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
